13cb3658edb6f3a31b23962e3d0e4a4f4881a6fc
zelkyyyy <maciej.owczarski@gmail.com>
2024-11-04 13:17:22 +0100

    part0-done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
 const Header = (props) => {
   console.log(props)
   return (
@@ -6,50 +10,57 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  let part=props.parts
+  let exercises = props.ex
   return (
-      <p>{props.part.name}{props.part.exercises}</p>
+      <p>{part}{exercises}</p>
   );
 }
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts} exercise={props.parts}/>
-      <Part part={props.parts} exercise={props.parts}/>
-      <Part part={props.parts} exercise={props.parts}/>
+      <Part part={props.parts} />
     </div>
   );
 }
 
 const Total = (props) => {
   return (
-      <p>{}</p>
+      <p>{props.exercises}</p>
   );
 }
 
 
 const App = () => {
+
   const course = {
     name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-   {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-   {
-      name: 'State of a component',
-      exercises: 14
+    part: [
+      {
+        text: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        text: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        text: 'State of a component',
+        exercises: 14
+      }
+    ],
+    ex: function() {
+      let sum = 0;
+      sum+=this.part[this.exercises]
+      return sum
     }
-  ]
   }
+  console.log(course.part)
   return (
     <div>
       <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts} />
+      <Content parts={course.part.text}/>
+      <Total exercises={course.ex} />
     </div>
   )
 }
